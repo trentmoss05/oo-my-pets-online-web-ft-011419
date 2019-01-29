d951b216d088729c43ff2926b3bf5e0489ba151d
Trent Moss <trentmoss05@gmail.com>
2019-01-29 14:51:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
   # code goes here
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@count = 1
+  @@all = []
+
+  def initialize(name)
+    @@count += 1
+    @@all << self
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.all
+    @@all
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+
+
+
+
+
 end
