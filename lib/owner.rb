4fc9f68e506ce0e37d3623f42899b4be87387076
Trent Moss <trentmoss05@gmail.com>
2019-01-29 15:05:57 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,28 +1,70 @@
 class Owner
   attr_accessor :pets, :name
   attr_reader :species
-  @@count = 1
-  @@all = []
+  OWNERS = []
 
-  def initialize(name)
-    @@count += 1
-    @@all << self
+  def initialize(species)
+    @species = species
+    OWNERS << self
     @pets = {fishes: [], cats: [], dogs: []}
   end
 
+  def self.reset_all
+    OWNERS.clear
+  end
+
   def self.count
-    @@count
+    OWNERS.size
   end
 
   def self.all
-    @@all
+    OWNERS
   end
 
   def buy_fish(name)
-    new_fish = Fish.new(name)
+    pets[:fishes] << Fish.new(name)
+  end
 
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
 
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
 
+  def say_species
+    "I am a #{@species}."
+  end
 
+  def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
 
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
 end
